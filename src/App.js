224b1dc8bd47e12a2d19373219c6e453637b8477
Clesio <kornalewski@outlook.com>
2024-06-06 08:04:58 -0300

    config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Test from './pages/Test';
+import Home from './pages/Home.jsx';
 
 function App() {
   return (
     <div className="App">
- <h1>Shey Money</h1>
+      
+      <BrowserRouter>
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/test' element={<Test />} />
+
+        </Routes>
+
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import DefaultLayout from '../components/DefaultLayout'
+
+const Home = () => {
+  return (
+    
+    <DefaultLayout>
+     <h1> Está é a home page</h1>
+    </DefaultLayout>
+  
+  )
+};
+
+export default Home;
